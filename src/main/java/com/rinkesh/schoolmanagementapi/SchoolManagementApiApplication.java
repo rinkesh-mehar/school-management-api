62a89fa55b113681432cc8b97cb411653ac0a5d3
Rinkesh Mehar <rinkesh.mehar@cropdata.in>
2020-10-23 19:14:05 +0530

    initial commit

---
+++ src/main/java/com/rinkesh/schoolmanagementapi/SchoolManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.rinkesh.schoolmanagementapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolManagementApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolManagementApiApplication.class, args);
+    }
+
+}
